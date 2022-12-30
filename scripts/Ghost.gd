e995038c715da7978c0d116e3d7fdc0202f7eb6f
PedroCosta9S <96085709+PedroCosta9S@users.noreply.github.com>
2022-12-30 13:00:11 +0000

    Spawner changes
    - It is now a KinematicBody
    - Pathfinding implemented in Spawner using NavigationAgent: It does not work yet probably do to the avaiable meshes in the Garden asset

--- scripts/Ghost.gd
+++ scripts/Ghost.gd
@@ -1,7 +1,6 @@
 extends KinematicBody
 
-onready var nav = $"../NavMeshInst" as Navigation
-onready var player = $"../Player" as KinematicBody
+
 
 
 var path = []
@@ -10,25 +9,25 @@ var speed = 2
 
 
 func _ready():
+	pass
+#	print("Agent ",agent)
+#	print("Nav: " ,nav)
+#	print("Player ",player)
 	# Generate path to the player
 	#update_path(player.global_transform.origin)
-	pass
-	
-
-
-func _physics_process(delta):
-	update_path(player.global_transform.origin)
-	if current_node < path.size():
-		var direction: Vector3 = path[current_node] - global_transform.origin
-		if direction.length() < 1:
-			current_node += 1
-		else:
-			move_and_slide(direction.normalized() * speed)
-
-func update_path(target_position):
-	path = nav.map_get_path(global_transform.origin, target_position)
-	
-
 	
 	
-	
+#func _physics_process(delta):
+	#update_path(player.global_transform.origin)
+#	if current_node < path.size():
+#		var direction: Vector3 = path[current_node] - global_transform.origin
+#		if direction.length() < 1:
+#			current_node += 1
+#		else:
+#			move_and_slide(direction.normalized() * speed)
+#
+#func update_path(target_position):
+#	path = nav.map_get_path(global_transform.origin, target_position)
+#
+#func _on_UpdatePath_timeout():
+#	update_path(player.global_transform.origin)
--- scripts/Spawner.gd
+++ scripts/Spawner.gd
@@ -1,7 +1,14 @@
-extends Spatial
+extends KinematicBody
 
 export(PackedScene) var Ghost
 onready var timer = $Timer
+
+#onready var nav = $"../NavMeshInst" as Navigation
+onready var agent : NavigationAgent = $NavigationAgent
+#onready var player = $"../Player" as KinematicBody
+onready var player : KinematicBody = $"../Player"
+
+export var speed = 5
 
 export var num_ghosts = 5
 export var second_between_spawns = 3
@@ -12,9 +19,29 @@ var ghosts_remaining_to_spawn
 
 
 func _ready():
+	
+	print("Agent ",agent)
+#	print("Nav: " ,nav)
+	print("Player ",player)
+	print("end")
+	
+	# Goes to Player Positon to attack him
+	agent.set_target_location(player.transform.origin)
+	
 	ghosts_remaining_to_spawn = num_ghosts
 	timer.wait_time = second_between_spawns
 	timer.start()
+	
+func _physics_process(delta):
+	# Gets the next position
+	var next_pos = agent.get_next_location()
+	# Work out the velocity
+	var velocity = (next_pos - transform.origin).normalized() * speed * delta
+	#print(velocity)
+	# Move to position
+	move_and_collide(velocity)
+	
+	
 
 func _on_Timer_timeout():
 	if ghosts_remaining_to_spawn:
